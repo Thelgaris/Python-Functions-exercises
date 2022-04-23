f0f3816e186f8a105efdbaf4443a45723ea3363e
Thelgaris <alex.gabarron84@gmail.com>
2022-04-23 15:55:09 +0000

    done

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,7 +1,7 @@
 # Your code goes here:
-def render_person(param):
-    return param
-
+def render_person(name, birth_year, color_eyes, age, gender):
+    sentence =  f'{name} is a {age} old {gender} born in {birth_year} with {color_eyes} eyes'
+    return sentence
 
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
